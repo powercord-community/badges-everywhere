e5747adf70d39c8e1891990e78e1331a946f9fbc
12944qwerty <qwerty12944qwerty@gmail.com>
2023-02-05 20:25:00 -0600

    bump replugged version

--- scripts/bundle.ts
+++
@@ -1,8 +0,0 @@
-import asar from "@electron/asar";
-import { readFileSync } from "fs";
-import { PluginManifest } from "replugged/dist/types/addon";
-
-const manifest = JSON.parse(readFileSync("manifest.json", "utf-8")) as PluginManifest;
-const outputName = `${manifest.id}.asar`;
-
-asar.createPackage("dist", outputName);
